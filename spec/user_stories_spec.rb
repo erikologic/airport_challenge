329f41462bbe124a1036ee7cec54ffdc7234ef7d
Enrico Graziani <mrenrich84@gmail.com>
2017-01-11 19:39:21 +0000

    User Story 5
    As an air traffic controller
    To ensure safety
    I want to prevent landing when the airport is full

--- spec/user_stories_spec.rb
+++ spec/user_stories_spec.rb
@@ -1,19 +1,29 @@
 describe 'User Stories' do
-#   As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport and confirm that it has landed
+  #  As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport and confirm that it has landed
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
-    expect{airport.land(plane)}.not_to raise_error
+    expect{ airport.land(plane) }.not_to raise_error
   end
 
-#   As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
-    expect{airport.take_off(plane)}.not_to raise_error
+    expect{ airport.take_off(plane) }.not_to raise_error
+  end
+
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent landing when the airport is full
+  it 'does not allow plane to land when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times{ airport.land(plane) }
+    expect{ airport.land(plane) }.to raise_error('Cannot land plane: airport full')
   end
 end
