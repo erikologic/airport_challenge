e41e77a967a6532078884e8557772cb0077d55bd
Enrico Graziani <mrenrich84@gmail.com>
2017-01-11 19:45:33 +0000

    Refactor of previous commit

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,22 @@ require 'airport'
 
 describe Airport do
   subject(:airport) {described_class.new(20)}
+  let(:plane) { double(:plane) }
 
+  describe '#land' do
     it 'instructs a plane to lane' do
       expect(airport).to respond_to(:land).with(1).argument
     end
-
+    context 'when at capacity' do
+      it 'raises an error' do
+        20.times{ airport.land(plane) }
+        expect{ airport.land(plane) }.to raise_error('Cannot land plane: airport full')
+      end
+    end
+  end
+  describe '#land' do
     it 'instructs a plane to take off' do
       expect(airport).to respond_to(:take_off).with(1).argument
     end
-
-    it 'does not allow landing when at capacity' do
-      plane = double(:plane)
-      20.times{ airport.land(plane) }
-      expect{ airport.land(plane) }.to raise_error('Cannot land plane: airport full')
-    end
+  end
 end
