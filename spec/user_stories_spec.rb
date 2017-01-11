ab9e9ef1b15a7252e4905cf49e2df0eba703c61b
Enrico Graziani <mrenrich84@gmail.com>
2017-01-11 20:09:45 +0000

    Refactor of previous commit

--- spec/user_stories_spec.rb
+++ spec/user_stories_spec.rb
@@ -1,43 +1,43 @@
 describe 'User Stories' do
-  #  As an air traffic controller
-  # So I can get passengers to a destination
-  # I want to instruct a plane to land at an airport and confirm that it has landed
-  it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new(20)
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return false
-    expect{ airport.land(plane) }.not_to raise_error
-  end
+  let(:airport) { Airport.new(20) }
+  let(:plane) { Plane.new }
 
-  # As an air traffic controller
-  # So I can get passengers on the way to their destination
-  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-  it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new(20)
-    plane = Plane.new
-    expect{ airport.take_off(plane) }.not_to raise_error
-  end
-
-  # As an air traffic controller
-  # To ensure safety
-  # I want to prevent landing when the airport is full
-  context 'when airport is full' do
-    it 'does not allow plane to land when airport is full' do
-      airport = Airport.new(20)
-      plane = Plane.new
+  context 'when not stormy' do
+    before do
       allow(airport).to receive(:stormy?).and_return false
-      20.times{ airport.land(plane) }
-      expect{ airport.land(plane) }.to raise_error('Cannot land plane: airport full')
+    end
+    #  As an air traffic controller
+    # So I can get passengers to a destination
+    # I want to instruct a plane to land at an airport and confirm that it has landed
+    it 'so planes land at airports, instruct a plane to land' do
+      expect{ airport.land(plane) }.not_to raise_error
+    end
+
+    # As an air traffic controller
+    # So I can get passengers on the way to their destination
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    it 'so planes take off from airports, instruct a plane to take off' do
+      expect{ airport.take_off(plane) }.not_to raise_error
+    end
+
+    context 'when airport is full' do
+    # As an air traffic controller
+    # To ensure safety
+    # I want to prevent landing when the airport is full
+      it 'does not allow plane to land when airport is full' do
+        20.times{ airport.land(plane) }
+        expect{ airport.land(plane) }.to raise_error('Cannot land plane: airport full')
+      end
     end
   end
+
+
 
   # As an air traffic controller
   # To ensure safety
   # I want to prevent landing when weather is stormy
   context 'when weather is stormy' do
     it 'does not allow planes to land when stormy' do
-      airport = Airport.new(20)
-      plane = Plane.new
       allow(airport).to receive(:stormy?).and_return true
       expect{ airport.land(plane) }.to raise_error('Cannot land plane: weather is stormy')
     end
